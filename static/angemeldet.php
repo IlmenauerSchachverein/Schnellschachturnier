2bb8cce894ee29cfe23f2a9cd7ae3e7349149e3d
Erik Skopp <skopp.erik@gmail.com>
2025-01-10 20:23:12 +0100

    Update angemeldet.php

--- static/angemeldet.php
+++ static/angemeldet.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anmeldungen Open 2025</title>
+    <title>ISST 2025</title>
     <style>
         table {
             width: 90%;
